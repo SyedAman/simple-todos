e3b75314a72f9ed2703cf9b70e6ba3eea17361b8
Syed Aman <sna274@nyu.edu>
2016-12-22 20:07:37 -0500

    render App.jsx to render-target div tag in main.html

--- client/main.jsx
+++ client/main.jsx
@@ -1,5 +1,8 @@
-// import MeteorJS
 import { Meteor } from 'meteor/meteor';
-// import React dependencies
 import { React } from 'react';
 import { render } from 'react-dom';
+
+Meteor.startup(() => {
+    // render App.jsx to render-target div tag in main.html
+    render(<App />, document.getElementById('render-target'));
+})
